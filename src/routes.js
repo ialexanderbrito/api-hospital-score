33d257cc0a0383ba402b084102e9a8713bb2a223
Alexander <xanderzinho26@gmail.com>
2020-07-21 19:08:15 -0300

    Adicionado novas rotas

--- src/routes.js
+++ src/routes.js
@@ -4,17 +4,15 @@ const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
 const HospitalController = require('./controllers/HospitalController');
-const UserController = require('./controllers/UserController');
 const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 const upload = multer(uploadConfig);
 
-routes.post('/users', UserController.store);
+routes.get('/hospitais', HospitalController.index);
+routes.get('/hospitais/:id', HospitalController.id);
+routes.post('/hospitais', upload.single('image_url'), HospitalController.store);
 
 routes.get('/search', SearchController.index);
-
-routes.get('/hospitais', HospitalController.index);
-routes.post('/hospitais', upload.single('image_url'), HospitalController.store);
 
 module.exports = routes;
